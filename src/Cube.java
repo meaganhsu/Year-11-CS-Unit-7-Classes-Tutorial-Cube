cdf4298d5d84867e4e1146e6a3ba7c910555dc3c
meaganhsu <myth72@student.cis.edu.hk>
2024-01-19 10:07:19 +0800

    done

---
+++ src/Cube.java
@@ -0,0 +1,23 @@
+public class Cube {
+    private int side;
+    public int getSide() {
+        return side;
+    }
+    public void setSide(int side) {
+        if (side > 1) this.side = side;
+        else throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+    }
+    public int getSurfaceArea () {
+        return 6 * (side * side);
+    }
+    public int getVolume () {
+        return (side * side * side);
+    }
+    public Cube() {
+        this.side = 1;
+    }
+    public Cube(int side) {
+        if (side > 1) this.side = side;
+        else throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+    }
+}
